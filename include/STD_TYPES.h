fc4aadee8766ef3a06684a656e6eac2e81728a17
Mahmoud Amr <38290949+mahmoudamr1205@users.noreply.github.com>
2021-01-25 23:40:38 +0200

    Create STD_TYPES.h

---
+++ include/STD_TYPES.h
@@ -0,0 +1,30 @@
+
+/*
+ * STD_TYPES.h
+ *
+ * Created: 10/1/2019 2:04:42 PM
+ *  Author: Mahmoud Amr
+ */ 
+
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+//+++++++++++++++++ STD +++++++++++++++++++++++++++++
+
+typedef  unsigned char			u8;
+typedef  signed char	       	s8;
+typedef  unsigned short int     u16;
+typedef  signed short          	sint16;
+typedef  unsigned long int      u32;
+typedef  signed long            sint32;
+typedef  unsigned long long 	uint64;
+typedef  signed long long       sint64;
+typedef  float               	float32;
+typedef  double           		float64;
+
+//+++++++++++++++++++++++++++++++++++++++++++++
+
+
+#endif /* STD_TYPES_H_ */
