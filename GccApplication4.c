8fd09643e149b2b76ac1a79f262493b2f7cd9fb5
abdullah-sawey <55079400+abdullah-sawey@users.noreply.github.com>
2021-02-05 06:30:36 +0200

    Update GccApplication4.c

--- GccApplication4.c
+++ GccApplication4.c
@@ -1,62 +1,103 @@
 /*
- * GccApplication4.c
+ * pwm.c
  *
  * Created: 1/31/2021 12:49:43 AM
  *  Author: abdul
  */ 
-
-
-#include <avr/io.h>
-#include <avr/delay.h>
 #define F_CPU 8000000ul
-
-int main(void)
-{
-	DDRD|= (1<<DDD6)|(1<<DDD5)|(1 << DDD3);
-	OCR0A =82;
-	OCR0B =82;
-	TCCR0A |= (1<<WGM00)|(1<<WGM01)|(1<<COM0A1)|(1<<COM0B1);
-	TCCR0B |= (1<<CS00)|(1<<CS02);
-	DDRB |= (1 << DDB1)|(1 << DDB2)|(1 << DDB3);
-	// PB1 and PB2 is now an output
-
-	//ICR1 = 0xFFFF;
-	// set TOP to 16bit
-
-	OCR1A = 64;
-	// set PWM for 25% duty cycle @ 16bit
-
-	OCR1B = 192;
-	// set PWM for 75% duty cycle @ 16bit
-
-	TCCR1A |= (1 << COM1A1)|(1 << COM1B1);
-	// set none-inverting mode
-
-	TCCR1A |= (1 << WGM10);
-	TCCR1B |= (1 << WGM12);//|(1 << WGM13);
-	// set Fast PWM mode using ICR1 as TOP
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/io.h>
+#define amp 5
+  void anlog_write(int pin , float value)
+  {
+	   
+	   float rduty_cycle;
+	   float duty_cycle ;
+	   switch (pin)
+	   {
+	   case 1:
+		  DDRD|= (1<<DDD6);
+		  rduty_cycle =0;
+		   duty_cycle =0;
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR0A = (duty_cycle*256) ;
+		  TCCR0A |= (1<<WGM00)|(1<<WGM01)|(1<<COM0A1);
+		  TCCR0B |= (1<<CS00)|(1<<CS02);
+		  break;
 	
-	TCCR1B |= (1 << CS10)|(1 << CS12);
-	// START the timer with 1024 prescaler
-	///////////////////////////////
-	OCR2A = 128;
-	OCR2B = 128;
-	// set PWM for 50% duty cycle
+	      case 2:
+		  
+		  DDRD|= (1<<DDD5);
+		  rduty_cycle =0;
+		  duty_cycle =0;
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR0B = (duty_cycle*256) ;
+		  TCCR0A |= (1<<WGM00)|(1<<WGM01)|(1<<COM0B1);
+		  TCCR0B |= (1<<CS00)|(1<<CS02);
+		  break;
+	  
+	 case 3:
+		 
+		  DDRB |= (1 << DDB1) ;
+		  rduty_cycle =0;
+		  duty_cycle =0;  
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR1A = (duty_cycle*256) ;
+		   TCCR1A |= (1 << COM1A1);
+		   TCCR1A |= (1 << WGM10);
+		   TCCR1B |= (1 << WGM12);
+		   TCCR1B |= (1 << CS10)|(1 << CS12);
+		   break;
+	  
+	 case  4:
+		  DDRB |= (1 << DDB2) ;
+		 rduty_cycle =0;
+		 duty_cycle =0;
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR1B = (duty_cycle*256) ;
+		  TCCR1A |= (1 << COM1B1);
+		  TCCR1A |= (1 << WGM10);
+		  TCCR1B |= (1 << WGM12);
+		  TCCR1B |= (1 << CS10)|(1 << CS12);
+		  break;
+	  
+	  case 5:
+		  
+		  DDRB |= (1 << DDB3) ;
+		 rduty_cycle =0;
+		 duty_cycle =0;
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR2A = (duty_cycle*256) ;
+		  TCCR2A |= (1 << COM2A1);
+		  TCCR2A |= (1 << WGM21) | (1 << WGM20);
+		  TCCR1B |= (1 << CS10)|(1 << CS12);
+		  TCCR2B |= (1 << CS20)| (1 << CS21)| (1 << CS22);
+		  break;
+	  
 
-
-	TCCR2A |= (1 << COM2A1)|(1 << COM2B1);
-	// set none-inverting mode
-
-	TCCR2A |= (1 << WGM21) | (1 << WGM20);
-	// set fast PWM Mode
-
-	TCCR2B |= (1 << CS20)| (1 << CS21)| (1 << CS22);
-	// set prescaler to 1024 and starts PWM
-	
-	
-	//while(1)
-	//{
-		//TODO:: Please write your application code
-	//}
-	
-}
+	  case 6:
+		 
+		  DDRD|= (1<<DDD3);
+		  rduty_cycle =0;
+		  duty_cycle =0;
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR2B = (duty_cycle*256) ;
+		  TCCR2A |= (1 << COM2B1);
+		  TCCR2A |= (1 << WGM21) | (1 << WGM20);
+		  TCCR1B |= (1 << CS10)|(1 << CS12);
+		  TCCR2B |= (1 << CS20)| (1 << CS21)| (1 << CS22);
+		  
+		   break;
+		   default:
+		   break;
+	  }
+	  
+	 
+  }
