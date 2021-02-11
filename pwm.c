c7c21617aa64722b76f2f0a947e3ee58ec67728c
abdullah-sawey <55079400+abdullah-sawey@users.noreply.github.com>
2021-02-11 02:06:11 +0200

    Add files via upload

---
+++ pwm.c
@@ -0,0 +1,102 @@
+/*
+ * pwm.c
+ *
+ * Created: 2/5/2021 1:25:20 AM
+ *  Author: abdul
+ */ 
+#define F_CPU 8000000UL
+#include "pwm.h"
+#include "std.h"
+#define amp 5
+ void setup(void){
+	  TCCR0A |= (1<<WGM00)|(1<<WGM01);
+	  TCCR1A |= (1 << COM1A1)|(1 << COM1B1);
+	  TCCR1A |= (1 << WGM10);
+	  TCCR1B |= (1 << WGM12);
+	  TCCR2A |= (1 << COM2A1)|(1 << COM2B1);;
+	  TCCR2A |= (1 << WGM21) | (1 << WGM20);
+	  TCCR2B |= (1 << CS20)| (1 << CS21)| (1 << CS22);
+	  TCCR0B |= (1<<CS00)|(1<<CS02);
+	  TCCR1B |= (1 << CS10)|(1 << CS12);
+
+ }
+ 
+ 
+  void anlog_write(int pin , float value)
+  {
+	   
+	   float rduty_cycle;
+	   float duty_cycle ;
+	   switch (pin)
+	   {
+	   case 1:
+	      SET_BIT( DDRD,DDD6)  ; 
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR0A = (duty_cycle*256) ;
+		 if (value!=0) {TCCR0A |= (1<<COM0A1);}
+		 if (value==0) {CLR_BIE(TCCR0A,COM0A1);}
+		  break;
+	
+	      case 2:
+		  
+		  SET_BIT( DDRD,DDD5)  ; 
+		  
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR0B = (duty_cycle*256) ;
+		 
+	   if (value!=0) {TCCR0A |= (1<<COM0B1);}
+       if (value==0) {CLR_BIE(TCCR0A,COM0B1);}
+		  break;
+	  
+	 case 3:
+		 
+	  SET_BIT( DDRB,DDB1)  ; 
+		
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR1A = (duty_cycle*256) ;  	 
+	  	 if (value!=0) {TCCR1A |= (1 << COM1A1);}
+  	     if (value==0) {CLR_BIE(TCCR1A,COM1A1);}
+	
+		  
+		   break;
+	  
+	 case  4:
+	  SET_BIT( DDRB,DDB2)  ; 
+		
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR1B = (duty_cycle*256) ;
+	   if (value!=0) {TCCR1A |= (1 << COM1B1);}
+       if (value==0) {CLR_BIE(TCCR1A,COM1B1);}
+		 
+		  break;
+	  
+	  case 5:
+		  SET_BIT( DDRB,DDB3)  ; 
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR2A = (duty_cycle*256) ;
+	      if (value!=0) {TCCR2A |= (1 << COM2A1);}
+          if (value==0) {CLR_BIE(TCCR2A,COM2A1);}
+		
+		  break;
+	  
+
+	  case 6:
+		  SET_BIT( DDRD,DDD3) ; 
+		 
+		  rduty_cycle = (value/ amp);
+		  duty_cycle =(rduty_cycle*rduty_cycle);
+		  OCR2B = (duty_cycle*256) ;
+	    if (value!=0) {TCCR2A |= (1 <<COM2B1);}
+        if (value==0) {CLR_BIE(TCCR2A,COM2B1);}
+		
+		   break;
+		   default:
+		   break;
+     
+	   }}	  
+	
---
+++ pwm.h
@@ -0,0 +1,171 @@
+/*
+ * pwm.h
+ *
+ * Created: 2/5/2021 1:27:35 AM
+ *  Author: abdul
+ */ 
+
+
+#ifndef PWM_H_
+#define PWM_H_
+#define OCR0A  (*(volatile unsigned char *) 0x47) 
+#define OCR0A_0 0
+#define OCR0A_1 1
+#define OCR0A_2 2
+#define OCR0A_3 3
+#define OCR0A_4 4
+#define OCR0A_5 5
+#define OCR0A_6 6
+#define OCR0A_7 7
+
+#define OCR0B  (*(volatile unsigned char *) 0x48)
+#define OCR0B_0 0
+#define OCR0B_1 1
+#define OCR0B_2 2
+#define OCR0B_3 3
+#define OCR0B_4 4
+#define OCR0B_5 5
+#define OCR0B_6 6
+#define OCR0B_7 7
+
+
+#define TCCR0A    (*(volatile unsigned char *) 0x44)
+#define WGM00 0
+#define WGM01 1
+#define COM0B0 4
+#define COM0B1 5
+#define COM0A0 6
+#define COM0A1 7
+
+#define TCCR0B (*(volatile unsigned char *) 0x45) 
+#define CS00 0
+#define CS01 1
+#define CS02 2
+#define WGM02 3
+#define FOC0B 6
+#define FOC0A 7
+
+#define DDRD (*(volatile  unsigned char *)0x2A) 
+#define DDD0 0
+#define DDD1 1
+#define DDD2 2
+#define DDD3 3
+#define DDD4 4
+#define DDD5 5
+#define DDD6 6
+#define DDD7 7
+
+
+#define DDRB (*(volatile  unsigned char *)0x24)
+#define DDB0 0
+#define DDB1 1
+#define DDB2 2
+#define DDB3 3
+#define DDB4 4
+#define DDB5 5
+#define DDB6 6
+#define DDB7 7
+
+#define OCR1A (*(volatile unsigned short *) 0x88)
+
+#define OCR1AL (*(volatile  unsigned char *) 0x88)  
+#define OCR1AL0 0
+#define OCR1AL1 1
+#define OCR1AL2 2
+#define OCR1AL3 3
+#define OCR1AL4 4
+#define OCR1AL5 5
+#define OCR1AL6 6
+#define OCR1AL7 7
+
+#define OCR1AH (*(volatile  unsigned char *)0x89)
+#define OCR1AH0 0
+#define OCR1AH1 1
+#define OCR1AH2 2
+#define OCR1AH3 3
+#define OCR1AH4 4
+#define OCR1AH5 5
+#define OCR1AH6 6
+#define OCR1AH7 7
+
+#define TCCR1A  (*(volatile  unsigned char *)0x80) 
+#define WGM10 0
+#define WGM11 1
+#define COM1B0 4
+#define COM1B1 5
+#define COM1A0 6
+#define COM1A1 7
+
+#define TCCR1B  (*(volatile unsigned char *)0x81)
+#define CS10 0
+#define CS11 1
+#define CS12 2
+#define WGM12 3
+#define WGM13 4
+#define ICES1 6
+#define ICNC1 7
+
+#define OCR1B (*(volatile unsigned short *) 0x8A)
+
+#define OCR1BL  (*(volatile  unsigned char *)0x8A) 
+#define OCR1BL0 0
+#define OCR1BL1 1
+#define OCR1BL2 2
+#define OCR1BL3 3
+#define OCR1BL4 4
+#define OCR1BL5 5
+#define OCR1BL6 6
+#define OCR1BL7 7
+
+#define OCR1BH  (*(volatile  unsigned char *)0x8B)
+#define OCR1BH0 0
+#define OCR1BH1 1
+#define OCR1BH2 2
+#define OCR1BH3 3
+#define OCR1BH4 4
+#define OCR1BH5 5
+#define OCR1BH6 6
+#define OCR1BH7 7
+
+
+#define OCR2A (*(volatile  unsigned char *)0xB3)
+#define OCR2_0 0
+#define OCR2_1 1
+#define OCR2_2 2
+#define OCR2_3 3
+#define OCR2_4 4
+#define OCR2_5 5
+#define OCR2_6 6
+#define OCR2_7 7
+
+#define OCR2B  (*(volatile unsigned char *)0xB4)
+#define OCR2_0 0
+#define OCR2_1 1
+#define OCR2_2 2
+#define OCR2_3 3
+#define OCR2_4 4
+#define OCR2_5 5
+#define OCR2_6 6
+#define OCR2_7 7
+
+
+#define TCCR2A  (*(volatile  unsigned char *)0xB0)
+#define WGM20 0
+#define WGM21 1
+#define COM2B0 4
+#define COM2B1 5
+#define COM2A0 6
+#define COM2A1 7
+
+#define TCCR2B (*(volatile  unsigned char *)0xB1)
+#define CS20 0
+#define CS21 1
+#define CS22 2
+#define WGM22 3
+#define FOC2B 6
+#define FOC2A 7
+
+void anlog_write(int pin , float value) ;
+ void setup(void) ;
+
+#endif /* PWM_H_ */
